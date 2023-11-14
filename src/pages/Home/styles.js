1504af4605154ec33337902cd04563193234f9d5
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-14 11:39:07 -0300

    create the aside and search styles

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-    backgrond-color: ${(props) => props.theme.colors.primary}
+export const Container = styled.aside`
+    background-color: ${(props) => props.theme.colors.background};
+    width: 360px;
+    height: 100vh;
+    overflow-y: auto;
+`;
+
+export const Search = styled.section`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    background-color: #fff;
+    padding: 16px;
 `;
