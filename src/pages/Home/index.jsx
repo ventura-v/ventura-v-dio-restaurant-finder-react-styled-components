5350b6afa4ef33a3a802edbaafd41aa233ddc385
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-14 11:40:18 -0300

    add the material text fields

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import TextField, { Input } from '@material/react-text-field';
 
-import Container from './styles'
+import { Container, Search } from './styles'
 
-const Home = () => <div>Hello World!</div>;
+import logo from '../../assets/logo.svg'
+
+const Home = () => {
+    const [inputValue, setInputValue] = useState('');
+
+    return (
+        <Container>
+            <Search>
+                <img src={logo} alt="Logo do site" />
+                <TextField
+                    label='Pesquisar'
+                    outlined
+                    //onTrailingIconSelect={() => this.setState({value: ''})}
+                    //trailingIcon={<MaterialIcon role="button" icon="delete"/>}
+                >
+                    <Input
+                        value={inputValue}
+                        onChange={(e) => setInputValue(e.target.value)} />
+                </TextField>
+            </Search>
+        </Container>
+    )
+};
 
 export default Home;
