14b56f502483e576eb57ae57df335f0e02c3791b
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-15 12:18:20 -0300

    Add the font to use in Material Icons

--- src/index.js
+++ src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import '@material/react-text-field/dist/text-field.css';
+import '@material/react-material-icon/dist/material-icon.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
