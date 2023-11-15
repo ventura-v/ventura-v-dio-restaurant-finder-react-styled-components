95a5953e72924e77eb64eea85f0b4f8dbd6f97ec
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-15 13:23:09 -0300

    Create carousel component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,13 +1,24 @@
 import React, { useState } from "react";
 import TextField, { Input } from '@material/react-text-field';
 import MaterialIcon from "@material/react-material-icon";
+import Slider from "react-slick";
 
-import { Container, Search, Logo, Wrapper } from './styles'
+import { Container, Search, Logo, Wrapper, CarouselTitle } from './styles'
 
 import logo from '../../assets/logo.svg'
+import restaurante from '../../assets/restaurante-fake.png'
 
 const Home = () => {
     const [inputValue, setInputValue] = useState('');
+
+    const settings = {
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        adaptiveHeight: true,
+    };
 
     return (
         <Wrapper>
@@ -23,6 +34,21 @@ const Home = () => {
                             value={inputValue}
                             onChange={(e) => setInputValue(e.target.value)} />
                     </TextField>
+                    <CarouselTitle>Na sua área</CarouselTitle>
+                    <Slider {...settings}>
+                        <div>
+                            <img src={restaurante} alt="" />
+                        </div>
+                        <div>
+                            <img src={restaurante} alt="" />
+                        </div>
+                        <div>
+                            <img src={restaurante} alt="" />
+                        </div>
+                        <div>
+                            <img src={restaurante} alt="" />
+                        </div>
+                    </Slider>
                 </Search>
             </Container>
         </Wrapper>
