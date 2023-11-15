0fa91a6d2522ab5912e8b8113cd57c6beabe92ca
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-15 14:52:48 -0300

    Create Card component

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+//exportando os componentes em um único arquivo, para não precisar importar posteriormente cada um de uma pasta diferente
+//agora basta importar este arquivo
+export { default as Card } from './ImageCard'
