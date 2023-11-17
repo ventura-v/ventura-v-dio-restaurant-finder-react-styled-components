58c1a505ebdee6855810467d887b9ed785f0ec28
Vinicius Ventura <vinicius__20@hotmail.com>
2023-11-17 19:57:30 -0300

    Skeleton component

---
+++ src/components/Skeleton/index.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled, { keyframes } from "styled-components";
+
+const KeyFrameLoading = keyframes`
+    0% {
+        opacity: 0.5;
+    }
+    100% {
+        opacity: 1;
+    }
+`;
+
+const LoadingSkeleton = styled.div`
+    background-color: #808080;
+    border-radius: 6px;
+    margin-bottom: 10px;
+    min-width: ${(props) => props.width};
+    height: ${(props) => props.height};
+    animation: ${KeyFrameLoading} 500ms infinite alternate;
+`;
+
+const Skeleton = ({ width, height }) => <LoadingSkeleton width={width} height={height} />;
+
+export default Skeleton;
